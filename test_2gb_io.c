badddadb4cdef973f1eb5ce81969ee6a2afd9fa8
Ed K <ed.karrels@gmail.com>
2018-09-19 12:28:28 -0500

    add 2-d test

--- test_2gb_io.c
+++ test_2gb_io.c
@@ -12,9 +12,11 @@
 #include "mpi.h"
 
 int rank = -1, np = -1;
-size_t size = 3000000000;
+size_t size = (size_t)3000 * 1024 * 1024;
 
 #define DEFAULT_FILENAME "test_2gb_io.tmp"
+
+int checkArray(const double *array, int count);
 
 
 int main(int argc, char **argv) {
@@ -22,8 +24,9 @@ int main(int argc, char **argv) {
   double *array = NULL;
   const char *filename = DEFAULT_FILENAME;
   MPI_File fh;
-  /* MPI_Datatype file_type, memory_type; */
+  MPI_Datatype array_type;
   MPI_Status status;
+  int starts[2] = {0,0}, sizes[2];
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
@@ -50,33 +53,47 @@ int main(int argc, char **argv) {
                 MPI_MODE_RDWR | MPI_MODE_CREATE,
                 MPI_INFO_NULL, &fh);
   
+  /* initialize array */
   for (i=0; i < count; i++)
     array[i] = i;
   
   MPI_File_set_view(fh, 0, MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL);
 
-  /* Try writing */
-  MPI_File_write(fh, array, count, MPI_DOUBLE, &status);
+  /* write 1-D array */
+  MPI_File_write_at(fh, 0, array, count, MPI_DOUBLE, &status);
   MPI_Get_count(&status, MPI_DOUBLE, &count_succeed);
   printf("Wrote %d of %d elements\n", count_succeed, count);
 
-  /* Try reading */
+  /* read 1-D array*/
   memset(array, 0, sizeof(double) * count);
   MPI_File_read_at(fh, 0, array, count, MPI_DOUBLE, &status);
   MPI_Get_count(&status, MPI_DOUBLE, &count_succeed);
   printf("Read %d of %d elements\n", count_succeed, count);
   
-  for (i=0; i < count; i++) {
-    if (array[i] != i) {
-      printf("Read error: array[%d] should be %e, but is %e\n",
-             i, (double)i, array[i]);
-      break;
-    }
-  }
+  checkArray(array, count);
 
+  /* write 2-D array */
+  sizes[0] = sqrt(count);
+  sizes[1] = count / sizes[0];
+  MPI_Type_create_subarray(2, sizes, sizes, starts,
+                           MPI_ORDER_C, MPI_DOUBLE, &array_type);
+  MPI_Type_commit(&array_type);
+
+  MPI_File_write_at(fh, 0, array, 1, array_type, &status);
+  MPI_Get_count(&status, array_type, &count_succeed);
+  printf("Wrote %d of 1 %dx%d array\n", count_succeed, sizes[0], sizes[1]);
+
+  /* read 2-D array */
+  memset(array, 0, sizeof(double) * count);
+  MPI_File_read_at(fh, 0, array, 1, array_type, &status);
+  MPI_Get_count(&status, array_type, &count_succeed);
+  printf("Read %d of 1 %dx%d array\n", count_succeed, sizes[0], sizes[1]);
+  checkArray(array, sizes[0] * sizes[1]);
+  
   MPI_File_close(&fh);
   
   if (rank == 0) remove(filename);
+  result = 0;
 
  fail:
   free(array);
@@ -84,3 +101,16 @@ int main(int argc, char **argv) {
   return result;
 }
  
+
+int checkArray(const double *array, int count) {
+  int i;
+  for (i=0; i < count; i++) {
+    if (array[i] != i) {
+      printf("Read error: array[%d] should be %e, but is %e\n",
+             i, (double)i, array[i]);
+      return 1;
+    }
+  }
+  return 0;
+}
+  
